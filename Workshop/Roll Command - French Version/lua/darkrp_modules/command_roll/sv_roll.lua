a3d6445cf8fb24c0192d4aa8ce11834858af4616
Kevin <34973659+keketiger@users.noreply.github.com>
2019-12-08 23:21:01 +0100

    minor fix

--- Workshop/Roll Command - French Version/lua/darkrp_modules/command_roll/sv_roll.lua
+++ Workshop/Roll Command - French Version/lua/darkrp_modules/command_roll/sv_roll.lua
@@ -1,20 +1,20 @@
 local function Roll(ply, args)
-	local say = function()
-		if GAMEMODE.Config.alltalk then
-			for _,target in pairs(player.GetAll()) do
-				DarkRP.talkToPerson(target, team.GetColor(ply:Team()), ply:Nick().. ' a tourné les dés et a fait ' .. math.random(1, 100) .. '.')
-			end
-		else
-			DarkRP.talkToRange(ply, ply:Nick().. ' a tourné les dés et a fait ' .. math.random(1, 100) .. '.', '', 250)
-		end
-	end
+     local say = function()
+          if GAMEMODE.Config.alltalk then
+               for _,target in pairs(player.GetAll()) do
+                    DarkRP.talkToPerson(target, team.GetColor(ply:Team()), ply:Nick().. ' a tourné les dés et a fait ' .. math.random(1, 100) .. '.')
+               end
+          else
+               DarkRP.talkToRange(ply, ply:Nick().. ' a tourné les dés et a fait ' .. math.random(1, 100) .. '.', '', 250)
+          end
+     end
 
-	return args, say
+     return args, say
 end
 
 DarkRP.defineChatCommand('roll', Roll, 1.5)
 DarkRP.declareChatCommand{
-	command = 'roll',
-	description = 'Vous permet d\'obtenir un nombre aléatoire entre 1 et 100.',
-	delay = 1.5
+     command = 'roll',
+     description = 'Vous permet d\'obtenir un nombre aléatoire entre 1 et 100.',
+     delay = 1.5
 }
